a541db61a5832479a0b01155e686bc1a90b878ec
GavDotZip <gavdotzip@gmail.com>
2024-02-09 00:08:41 +0000

    commit

--- src/A2/RandomPerson.java
+++ src/A2/RandomPerson.java
@@ -1,4 +1,49 @@
 package A2;
 
 public class RandomPerson {
+
+    private int age;
+    private String name;
+    private char gender;
+
+    public RandomPerson(){
+        this.age = 0;
+        this.name = "Unknown";
+        this.gender = '?';
+    }
+
+    public RandomPerson(String personName, int personAge, char personGender){
+        this.name = personName;
+        this.age = personAge;
+        this.gender = personGender;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int personAge) {
+        this.age = personAge;
+    }
+
+    public void setGender(char personGender){
+        this.gender = personGender;
+    }
+
+    public void setName(String personName){
+        this.name = personName;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " " + getAge() + " " + getGender();
+    }
 }
--- src/A2/RandomPersonTester.java
+++ src/A2/RandomPersonTester.java
@@ -1,4 +1,39 @@
 package A2;
 
 public class RandomPersonTester {
+
+    public static void main(String[] args){
+
+        RandomPerson[] personList = new RandomPerson[2];
+
+        System.out.println("Creating people...");
+
+        for(int i=0; i < personList.length; i++){
+            RandomPerson newPerson = new RandomPerson();
+            personList[i] = newPerson;
+        }
+
+        System.out.println("Printing contents of array...");
+
+        for (RandomPerson randomPerson : personList) {
+            System.out.println(randomPerson.toString());
+        }
+
+        System.out.println("Modifying contents of array...");
+
+        for (RandomPerson randomPerson : personList) {
+            randomPerson.setName("Edgar Allen Pug");
+            randomPerson.setAge(5);
+            randomPerson.setGender('M');
+        }
+
+        System.out.println("Printing contents of modified array...");
+
+        for (RandomPerson randomPerson : personList) {
+            System.out.println(randomPerson.toString());
+        }
+
+
+
+    }
 }
