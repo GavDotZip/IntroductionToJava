b1db033605f8498a1c9712d69f9f2b44edfa5f4c
GavDotZip <gavdotzip@gmail.com>
2024-02-09 00:14:24 +0000

    commit

--- src/A2/RandomPersonTester.java
+++ src/A2/RandomPersonTester.java
@@ -1,39 +1,53 @@
+// Define a package named A2
 package A2;
 
+// Define a class named RandomPersonTester
 public class RandomPersonTester {
 
+    // Main method, entry point of the program
     public static void main(String[] args){
 
-        RandomPerson[] personList = new RandomPerson[2];
+        // Create an array to store RandomPerson objects
+        RandomPerson[] personList = new RandomPerson[3];
 
-        System.out.println("Creating people...");
+        // Inform user about creating people
+        System.out.println("Creating Array");
 
+        // Loop through the personList array
         for(int i=0; i < personList.length; i++){
+            // Create a new RandomPerson object
             RandomPerson newPerson = new RandomPerson();
+            // Assign the newly created RandomPerson object to the array
             personList[i] = newPerson;
         }
 
-        System.out.println("Printing contents of array...");
+        // Inform user about printing the contents of the array
+        System.out.println("Printing Array");
 
+        // Loop through the personList array using enhanced for loop
         for (RandomPerson randomPerson : personList) {
+            // Print the string representation of each RandomPerson object
             System.out.println(randomPerson.toString());
         }
 
-        System.out.println("Modifying contents of array...");
+        // Inform user about modifying the contents of the array
+        System.out.println("Altering Array");
 
+        // Loop through the personList array using enhanced for loop
         for (RandomPerson randomPerson : personList) {
-            randomPerson.setName("Edgar Allen Pug");
-            randomPerson.setAge(5);
+            // Modify the attributes of each RandomPerson object
+            randomPerson.setName("Phoenix Wright");
+            randomPerson.setAge(30);
             randomPerson.setGender('M');
         }
 
-        System.out.println("Printing contents of modified array...");
+        // Inform user about printing the contents of the modified array
+        System.out.println("Printing New Array");
 
+        // Loop through the personList array using enhanced for loop
         for (RandomPerson randomPerson : personList) {
+            // Print the string representation of each modified RandomPerson object
             System.out.println(randomPerson.toString());
         }
-
-
-
     }
 }
